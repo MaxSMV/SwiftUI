37c6d8c92b10e28b37331dc14396171fb8437915
MaxSMV <maxstefankiv2001@gmail.com>
2023-11-01 15:53:03 +0200

    Card Component

---
+++ Hike-App/App/Hike/Hike/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Max Stefankiv on 01.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike-App/App/Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Max Stefankiv on 01.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
