54abc3ce883e1421be9565e659698023f7f9676d
MaxSMV <maxstefankiv2001@gmail.com>
2023-11-12 20:31:20 +0200

    Button Style

--- Hike-App/App/Hike/Hike/Utility/GradientButtonStyle.swift
+++ Hike-App/App/Hike/Hike/Utility/GradientButtonStyle.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct GradientButton:ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .padding(.vertical)
+            .padding(.horizontal, 30)
+            .background(
+                // Conditional Statment with Nil Coalescing
+                // Conditional ? A : B
+                configuration.isPressed ?
+                // A: When User pressed the Button
+                LinearGradient(colors: [.customGrayMedium, .customGrayLight], startPoint: .top, endPoint: .bottom)
+                :
+                // B: When the Button in not pressed
+                LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom)
+                )
+            .cornerRadius(40)
+    }
+}
